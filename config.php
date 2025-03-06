2d79242c1f8a0fe6c52b7afe70e3a498163cd74b
srkraut <sarojraut6@gmail.com>
2025-03-06 14:11:16 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+        'theme_name' => 'Signage Shield',
+		'theme_title' => 'Signage Shield',
+		'app_name' => 'Signage Shield',
+		'theme_url' => 'https://cms.signageshield.com/',
+		'cms_source_url' => 'https://cms.signageshield.com/',
+		'cms_install_url' => 'https://cms.signageshield.com/',
+		'cms_release_notes_url' => 'https://cms.signageshield.com/',
+		'view_path' => '../web/theme/custom/views'
+	);
